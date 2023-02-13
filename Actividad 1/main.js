84505e27600c3a433d46b6bf5d19c22d72a4121a
Gusta1075 <gustavosanmartino.gs@gmail.com>
2023-02-13 20:23:08 +0100

    cambios

---
+++ Actividad 1/main.js
@@ -0,0 +1,17 @@
+/*let pedirNumero = prompt("por favor colocar un numero");
+    alert("su numero es:" + pedirNumero);
+
+    function suma(numero1, numero2){
+        return = numero1 + nuemro2;
+    }
+*/
+    //ciclo for
+    for (numeros= 1; numeros <200; numeros++) {
+        console.log(numeros);
+    }
+    // ciclo while
+
+    var i = 10;
+    while (i <10), i++; {
+        alert(i);
+    }
